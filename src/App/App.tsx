08228aeaf87fc12928080ae6dc53dd1586e4e641
CJL Fortunato <clemence.fortunato@gmail.com>
2022-01-27 17:26:24 +0100

    Set up project

---
+++ src/App/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App(): JSX.Element {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Goblin Market</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
